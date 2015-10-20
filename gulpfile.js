7be227a8e4433058b3701809d3180bf0d0fa917b
kaatula <andrey.kuleshov@devexpress.com>
2015-10-20 15:55:19 +0300

    nit

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+﻿var gulp = require("gulp"),
+    svgstore = require("gulp-svgstore");
+
+gulp.task("default", ["concat_icons"], function () {
+    // place code for your default task here
+});
+
+
+gulp.task("concat_icons", function () {
+    return gulp
+        .src("./icons/**/*.svg")
+        .pipe(svgstore())
+        .pipe(gulp.dest("."));
+});
