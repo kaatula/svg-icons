3c7923460e8b9cf4e73f9c51ad91b0ceffee9a06
kaatula <andrey.kuleshov@devexpress.com>
2015-10-21 12:22:47 +0300

    Add styling and styling

--- svg-loader.js
+++ svg-loader.js
@@ -1,4 +1,3 @@
-
 var embedSvg = function () {
     var $iconsLink = $("link[rel=svg-icons]");
     if($iconsLink.length != 1) {
@@ -8,8 +7,10 @@ var embedSvg = function () {
         document.body.appendChild(content.childNodes[1]);
     });
 }
-//embedSvg();
 
-$.get("/icons.svg").done(function(content) {
-    document.body.appendChild(content.childNodes[1]);
-});
+embedSvg();
+
+//Или просто захардкодить...
+//$.get("icons.svg").done(function(content) {
+//    document.body.appendChild(content.childNodes[1]);
+//});
